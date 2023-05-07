e8c8747a09a34eeec71dc5dff1fe9ec3ffeb6538
Crying Nights <96980396+CryingN@users.noreply.github.com>
2023-05-07 21:19:08 +0800

    Add files via upload

--- code/main.gd
+++ code/main.gd
@@ -1,14 +1,79 @@
 extends Node2D
 
+var A_team_color:Color = Color(0.73,0,0,1)
+var B_team_color:Color = Color(0.3,0.2,0.79,1)
+var num_ma_in:int = 0
+
+var player = preload('res://scene/player.tscn')
+
 onready var Ma_in = $UI/Ma_in
 onready var A_team = $UI/A_team
 onready var B_team = $UI/B_team
+onready var delete = $UI/delete
 
 func _ready():
-	# 添加测试
+	# test place
 	Ma_in.add_item("miao",2)
-	print(Ma_in.get_item_text(2))
-	A_team.modulate = Color(0,0,0,1)
-	B_team.modulate = Color(1,1,1,1)
+	print(bool(Ma_in.get_item_text(2) == "miao"))
 	
+	# _ready() place
+	A_team.modulate = A_team_color
+	B_team.modulate = B_team_color
 	pass
+
+# A_team button, it used to create new player.tscn.
+func _on_A_team_pressed():
+	var the_player = player.instance()
+	the_player.the_color = A_team_color
+	the_player.number = 233
+	the_player.position = Vector2(100,100)
+	add_child(the_player)
+	pass
+
+# B_team button, like _on_A_team_pressed().
+func _on_B_team_pressed():
+	var the_player = player.instance()
+	the_player.the_color = B_team_color
+	the_player.number = 234
+	the_player.position = Vector2(700,1300)
+	add_child(the_player)
+	pass
+
+# arrow checkbutton
+## true is arrow mode
+## false is station mode
+func _on_arrow_toggled(button_pressed):
+	if button_pressed:
+		Global.station = false
+	else:
+		Global.station = true
+	pass
+
+
+
+func _on_station_toggled(button_pressed):
+	if button_pressed:
+		pass
+	pass
+	
+# save
+func _on_save_pressed(_name):
+	if num_ma_in == 0:
+		Ma_in.add_item("miao",2)
+		Global.pt("# create data {key} in Ma_in".format({key = "name"}))
+	print(bool(Ma_in.get_item_text(2) == "miao"))
+	pass
+
+# if the player will be delete, be red.
+func _on_delete_pressed():
+	if Global.station:
+		delete.modulate = Color(1,0,0,0.78)
+		Global.clean = true
+	pass
+
+# drag here to delete the player.
+func _on_delete_released():
+	delete.modulate = Color(1, 1, 1, 1)
+	Global.clean = false
+	pass
+
--- code/player.gd
+++ code/player.gd
@@ -1,24 +1,52 @@
-extends TouchScreenButton
+extends Node2D
 
+onready var circle = $Button
 onready var text = $name
 onready var label = $number
-onready var circle = $circle
 
-var the_name
+var number : int = 233
+var can_move : bool = false
 var the_color : Color
 
 func _ready():
-	the_name = "hahaha"
-	#write_the_number(the_name)
-	move_color(the_color)
+	modulate = the_color
+	label.text = str(number)
+	print("new")
 	pass
 
-func write_the_number(name):
+# write the player's name
+func write_the_name(name):
 	text.text = ""
 	text.append_bbcode(
 		"[center][b]{num}[/b][/center]".format({"num" : name})
 	)
 
-func move_color(color:Color):
-	circle.modulate = color
+# loop to use get point position.
+func _process(_delta):
+	if can_move:
+		position = get_global_mouse_position()	
 	pass
+
+# let player down
+func _on_Button_pressed():
+	if not Global.clash and Global.station:
+		can_move = true
+		Global.clash = true
+		modulate = the_color + Color(0.1,0.1,0.1,0)
+		Global.pt("let {num} down".format({num = number}))
+	pass
+
+# up,up,up
+func _on_Button_released():
+	if Global.clash and Global.station:
+		Global.clash = false
+		can_move = false
+		modulate = the_color
+		Global.pt("let {num} up".format({num = number}))
+		print(Global.clean)
+		if Global.clean:
+			queue_free()
+			Global.clean = false
+			Global.pt("{num} is free".format({num = number}))
+	pass
+
